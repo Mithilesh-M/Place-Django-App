a7ac729c87ee30057b71a9c70bfbb1252e0d10bd
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-09 15:31:21 +0530

    Add index view in place app
    Add this for place app homepage

--- placeapp/admin.py
+++ placeapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Place, Location, City
+from django.contrib.gis.admin import OSMGeoAdmin
 
 admin.site.register(Location)
 admin.site.register(City)
--- placeapp/views.py
+++ placeapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Location, Place, City
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    num_places = Place.objects.all().count()
+    num_cities = City.objects.all().count()
+
+    context = {
+        'num_places': num_places,
+        'num_cities': num_cities,
+    }
+
+    return render(request, 'index.html', context=context)
